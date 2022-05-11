300a980964665277d304b7e2127b6383781fa921
Datuchaaa <vashaymadzedavit@gmail.com>
2022-05-11 14:11:01 +0400

    fix: fix burger bar bug

--- pages/main/script.js
+++ pages/main/script.js
@@ -15,5 +15,11 @@ hamburger.addEventListener("click", () => {
     
 })
 
+document.querySelectorAll(".nav-link").forEach(n => n.
+addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }))
+
 
 
