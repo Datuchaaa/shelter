84e1cc2c55859a12b907ccf5859a2ef6ccd0a506
Datuchaaa <vashaymadzedavit@gmail.com>
2022-05-11 15:25:00 +0400

    fix: fix burger bar bug

--- pages/main/script.js
+++ pages/main/script.js
@@ -15,11 +15,25 @@ hamburger.addEventListener("click", () => {
     
 })
 
-document.querySelectorAll(".nav-link").forEach(n => n.
-addEventListener("click", () => {
-        hamburger.classList.remove("active");
-        navMenu.classList.remove("active");
-    }))
+
+// let arr = ['<div><?',6,7,7];
+
+document.querySelectorAll('.nav-link').forEach((value)=>{
+    value.addEventListener('click', ()=>{
+        console.log('click test');
+        navMenu.classList.toggle("active");
+    })
+});
+document.querySelectorAll('.container').forEach((vlll)=>{
+    vlll.addEventListener('click', () =>{
+        // navMenu.remove("active");
+    })
+});
+
+
+// document.querySelectorAll(".nav-link").forEach(n => n.
+//     addEventListener("click", () => {navMenu.classList.toggle("active");
+// }));
 
 
 
